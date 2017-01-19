e9266c05b45ef40a0b27059246cbc07fe5031110
Nabil D <nabildroussi@gmail.com>
2017-01-19 19:24:55 +0000

    First version.

---
+++ bootstrap-alert-quick.js
@@ -0,0 +1,45 @@
+﻿(function ($) {
+    $.fn.bootstrapAlert = function (options) {
+        var settings = $.extend({
+            "type": "success",
+            "showIcon": true,
+            "icon": "exclamation-circle",
+            "showCloseButton": true
+        }, options);
+
+        this.filter("div").each(function () {
+            $containerDiv = $("<div></div>");
+
+            $containerDiv.addClass("alert fade in alert-" + settings.type);
+
+            if (settings.showIcon === true) {
+                $icon = $("<i></i>")
+                        .addClass("fa fa-" + settings.icon)
+                        .css("margin-right", "5px");
+
+                $icon.appendTo($containerDiv);
+            }
+
+            if (settings.showCloseButton === true) {
+                $closeButton = $('<button />');
+
+                $closeButton.addClass("close close-sm")
+                    .attr("type", "button")
+                    .data("dismiss", "alert");
+
+                $btnIcon = $("<i></i>")
+                        .addClass("fa fa-times");
+
+                $btnIcon.appendTo($closeButton);
+
+                $closeButton.appendTo($containerDiv)
+            }
+
+            $containerDiv.append(settings.content);
+
+            $containerDiv.appendTo(this);
+        });
+
+        return this;
+    }
+}(jQuery));
